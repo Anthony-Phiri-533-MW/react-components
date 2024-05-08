f32b70efe5522ab34c0bc8b119ae8d3aa23fabb3
Anthony-Phiri-533-MW <anthonyphiri533@gmail.com>
2024-05-09 00:57:04 +0200

    "Update Navbar: change text in links and logo"
    This commit updates the navigation bar by changing the text in some of the links and the logo. The "Projects", "About", "Contact", and "Blog" links

--- navbar/Navbar.jsx
+++ navbar/Navbar.jsx
@@ -1,29 +1,27 @@
-"use client"
-
 import React, { useState } from 'react'
 import Link from 'next/link'
 
 const Navbar = () => {
-    const [navbar, setNavbar] = useState(false)
+    
   return ( 
     <nav className="flex justify-between z-10">
-        <p className='text-xl'>DataIntMw</p>
+        <p className='text-xl'>Logo</p>
 
         <ul className='flex'>
             <li className='pr-10 text-xl'>
-                <Link href='#'>Projects</Link>
+                <Link href='#projects'>Projects</Link>
             </li>
 
             <li className='pr-10 text-xl'>
-                <Link href='#'>About</Link>
+                <Link href='#about'>About</Link>
             </li>
 
             <li className='pr-10 text-xl'>
-                <Link href='#'>Contact</Link>
+                <Link href='contact'>Contact</Link>
             </li>
 
             <li className='pr-1 text-xl'>
-                <Link href='#'>Blog</Link>
+                <Link href='#blog'>Blog</Link>
             </li>
         </ul>
     </nav>
